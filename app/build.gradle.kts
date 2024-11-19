5e78fa3ad4b1736aaa313c2eb7aca5435f154948
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2024-11-19 09:09:27 -0300

    :sparkles: feat: Config splashscreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.constraintlayout)
     ksp(libs.androidx.room.compiler)
     implementation(libs.hilt.android)
@@ -86,6 +87,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.core.splashscreen)
 }
 
 // Allow references to generated code
